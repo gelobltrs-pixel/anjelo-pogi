b339f3001f3d8f4e703f6ad794eb3eab1c808477
gelobltrs-pixel <gelobltrs@gmail.com>
2026-08-04 10:31:11 +0800

    Add files via upload

---
+++ anjelo.py
@@ -0,0 +1,49 @@
+#RECORD = 75
+#IF RECORD >= 90
+#PRINT("EXCELLENT")
+#ELIF RECORD >= 75:
+   # PRINT("PASSED")
+  #  ELSE:
+    #    PRINT("NEED IMPROVEMENT")
+#SCORES +[90, 55, 87, 100, 99]
+
+
+
+#print('SUBMIT NEW REPORT')
+#('REVIEW ALL SUBMIT REPORT')
+#PRINT()
+#print()
+
+def record
+REPORTNAME = ANJELO
+REPORT = 2024-28-2001
+ICIDENT =  LAB DAMAGE
+PRIORITY = HIGH
+DECRIPTION = THE COMPUTER LAB NOT WORKING
+EVIDENCE= true
+CURRENT= STATUS
+
+
+
+print("REPORTNAME")
+print("REPORT")
+print("ICIDENT")
+print("PRIORITY")
+print("DECRIPTION")
+print("EVIDENCE")
+print("CURRENT")
+
+
+
+
+
+
+
+ 
+
+
+
+
+
+             
+
